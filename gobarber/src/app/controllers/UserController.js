1df5ff84d6d866dd08304103809d0ed70a29f95f
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-02-02 14:44:12 -0400

    [Learning][UserController.js] - Added Yup to check fields

--- gobarber/src/app/controllers/UserController.js
+++ gobarber/src/app/controllers/UserController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(request, response) {
+    // check fields
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
     // verify if exist a user in DB
     const userExist = await User.findOne({
       where: { email: request.body.email },
@@ -25,6 +39,25 @@ class UserController {
   }
 
   async update(request, response) {
+    // check fields
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = request.body;
 
     const user = await User.findByPk(request.userId);

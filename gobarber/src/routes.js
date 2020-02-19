57461b67dcb5a005402df25eb634cffc02ab97f7
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-02-18 20:11:30 -0400

    [Appointment.js] - Creating entity appoitment in DB

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import multer from 'multer';
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/authMiddleware';
 import multerConfig from './config/multer';
+
+/**
+ * controllers
+ */
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +24,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

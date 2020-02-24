a52367431f14a52f59896f2a38ebf5c9a53d2084
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-02-24 13:38:40 -0400

    [AppointmentController.js][NotificationController.js][Notification.js] - Created notification when provider add new appointment using mongodb

---
+++ gobarber/src/app/controllers/NotificationController.js
@@ -0,0 +1,36 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(request, response) {
+    // check if provider_id is a provider
+    const checkIsProvider = await User.findOne({
+      where: { id: request.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return response
+        .status(401)
+        .json({ error: 'Only providers can load notifications' });
+    }
+
+    const notifications = await Notification.find({
+      user: request.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return response.json(notifications);
+  }
+
+  async update(request, response) {
+    const notification = await Notification.findByIdAndUpdate(
+      request.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return response.json(notification);
+  }
+}
+export default new NotificationController();
--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -11,7 +11,8 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-import ScheduleController from "./app/controllers/ScheduleController";
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -30,6 +31,9 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

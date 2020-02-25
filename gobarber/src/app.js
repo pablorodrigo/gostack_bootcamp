904439495f38376a1b6145f45aa3583635b7007e
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-02-25 19:59:46 -0400

    [sentry.js] - Added exception handler using sentry.io

--- gobarber/src/app.js
+++ gobarber/src/app.js
@@ -4,19 +4,30 @@
 
 import express from 'express';
 import path from 'path';
+// it is a good exceptions' view for developer
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     // calling methods
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   // register of all middlewares in your project
   middlewares() {
+    // sentry must be configured before routes
+    this.server.use(Sentry.Handlers.requestHandler());
     // handle to receiver form json data
     this.server.use(express.json());
     this.server.use(
@@ -27,6 +38,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // middleware for exception handler
+    this.server.use(async (error, request, response, next) => {
+      const errors = await new Youch(error, request).toJSON();
+
+      return response.status(500).json(errors);
+    });
   }
 }
 
---
+++ gobarber/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://de8a0d5e85504d9baa764bf1dc36898d@sentry.io/2909397',
+};

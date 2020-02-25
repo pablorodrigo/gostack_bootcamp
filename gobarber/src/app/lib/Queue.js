fd06f5ec408a44c2a5b7baa2e3c6b671ec50b3a8
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-02-24 21:03:32 -0400

    [Queue.js][redis.js] - Config queues with Redis for emals sended. Better performance

---
+++ gobarber/src/app/lib/Queue.js
@@ -0,0 +1,50 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../jobs/CancellationMail';
+import redisConfig from '../../config/redis';
+
+// each job needs an unique key
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        // it is a instance witch communicate the redis and save the data in DB
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        // process each queue (line), receive the variables by our context
+        handle,
+      };
+    });
+  }
+
+  /**
+   * add new jobs in the queue separately for each email
+   * @param queue
+   * @param job -> our data
+   * @returns {*}
+   */
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  /**
+   * get each queue and process in background
+   */
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
---
+++ gobarber/src/queue.js
@@ -0,0 +1,6 @@
+/**
+ * This way it will run separate our main app. Which means, it will not impact on performance.
+ */
+import Queue from './app/lib/Queue';
+
+Queue.processQueue();

175aedfba12f292ff8b559ac23bc6d4ccde911a3
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-03-08 19:20:02 -0400

    [Course][ReactJS][WebModule] - Initial setup

---
+++ gobarber_web/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './App.css'
+
+import profile from './assets/20181116_095244.jpg'
+
+function App() {
+    return <img width="50px" src={profile}/>
+}
+
+export default App;

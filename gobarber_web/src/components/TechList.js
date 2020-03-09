42297c3258ec8d9c01c319c1379bb68c6bee6c1b
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-03-09 18:03:12 -0400

    [Course][ReactJSIntro][WebModule] - State & Immutability

--- gobarber_web/src/components/TechList.js
+++ gobarber_web/src/components/TechList.js
@@ -1,26 +1,56 @@
 import React, {Component} from "react";
 
-class TechList extends Component{
+class TechList extends Component {
 
     state = {
-      techs: [
-          'Node.js',
-          'ReactJS',
-          'React Native'
-      ]
+        newTech: '',
+        techs: [
+            'Node.js',
+            'ReactJS',
+            'React Native'
+        ]
+    };
+
+    // arrow function is required to access 'this' variable
+    handleInputChange = e => {
+
+        //console.log(e.target.value);
+        this.setState({newTech: e.target.value})
+
+    };
+
+    handleSubmit = e => {
+        // avoid update screen
+        e.preventDefault();
+
+
+        this.setState({
+            // copy the previous list and create new
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: ''
+        })
+
+        //console.log(this.state.newTech)
     };
 
     // method required
     render() {
 
-        console.log(this.state);
 
         return (
-            <ul>
-                <li>NodeJS</li>
-                <li>ReactJS</li>
-                <li>ReactNative</li>
-            </ul>
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <ul>
+                        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                    </ul>
+
+                    <input type="text"
+                           onChange={this.handleInputChange}
+                           value={this.state.newTech}/>
+
+                    <button type="submitte">Enviar</button>
+                </form>
+            </>
         )
     }
 

903af2801eed6b3b35c3dd3531e617a199dce680
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-03-12 22:39:27 -0400

    [InitialSetup] - Configured global styles

--- first_reactjs_project/src/pages/Main/styles.js
+++ first_reactjs_project/src/pages/Main/styles.js
@@ -2,13 +2,5 @@ import styled from 'styled-components';
 
 // example of creating your own component
 export const Title = styled.h1`
-    font-size: 20px;
-    //control properties
-    color: ${props => (props.error ? 'red' : '#7159c1')};
-    font-family: Arial, Helvetica, sans-serif;
-
-    small {
-        font-size: 10px;
-        color: #333;
-    }
+    color: #fff;
 `;

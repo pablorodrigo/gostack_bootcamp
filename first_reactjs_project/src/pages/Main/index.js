614e60815e465838fa8bb5a9de1cbf1381b4f242
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-03-17 22:25:41 -0400

    [FirstProjectReact][Learning] - Added git repositories

--- first_reactjs_project/src/pages/Main/index.js
+++ first_reactjs_project/src/pages/Main/index.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-    return (
-        <Container>
-            <h1>
-                <FaGithubAlt />
-                Repository
-            </h1>
+import api from '../../services/api';
 
-            <Form onSubmit={() => {}}>
-                <input type="text" placeholder="Add repo" />
+export default class Main extends Component {
+    state = {
+        newRepo: '',
+        repositories: [],
+        loading: false,
+    };
 
-                <SubmitButton>
-                    <FaPlus color="#FFF" size={14} />
-                </SubmitButton>
-            </Form>
-        </Container>
-    );
+    handleInputChange = event => {
+        this.setState({ newRepo: event.target.value });
+    };
+
+    handleSubmit = async event => {
+        // avoid page refresh
+        event.preventDefault();
+
+        this.setState({ loading: true });
+
+        const { newRepo, repositories } = this.state;
+
+        const response = await api.get(`/repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        this.setState({
+            repositories: [...repositories, data],
+            newRepo: '',
+            loading: false,
+        });
+    };
+
+    render() {
+        const { newRepo, loading } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repository
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Add repo" value={newRepo} onChange={this.handleInputChange} />
+
+                    <SubmitButton loading={loading}>
+                        {loading ? <FaSpinner color="#FFF" size={14} /> : <FaPlus color="#FFF" size={14} />}
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }

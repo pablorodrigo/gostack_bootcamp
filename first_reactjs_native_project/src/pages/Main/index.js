f27f2ed2fd12ce74fb497e8bfd46aa33f3668f99
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-08 10:54:50 -0400

    [FirstProjectReactNative][Learning] - Acessing Github API

--- first_reactjs_native_project/src/pages/Main/index.js
+++ first_reactjs_native_project/src/pages/Main/index.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main({ navigation }) {
-  function thisToUsers() {
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    console.tron.log(this.state.newUser);
+
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  /* function thisToUsers() {
     navigation.navigate('User');
+  } */
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorret={false}
+            autoCapitalize="none"
+            placeholder="Add user"
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+
+        {/* <Button title="Navigate to Users" onPress={thisToUsers} /> */}
+      </Container>
+    );
   }
-
-  return (
-    <Container>
-      <Form>
-        <Input autoCorret={false} autoCapitalize="none" placeholder='Add user' />
-
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-
-      <Button title="Navigate to Users" onPress={thisToUsers} />
-    </Container>
-  );
 }

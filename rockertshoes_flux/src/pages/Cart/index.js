00e218dc5a2d8a08adca2ad95f69a8302cee9747
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-17 23:55:33 -0400

    [ReactJS][Learning Redux] - Added project structure, routes configuration and global styles

---
+++ rockertshoes_flux/src/pages/Cart/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    render() {
+        return <div>test</div>;
+    }
+}
+
+export default Cart;
---
+++ rockertshoes_flux/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import { } from './styles';
+
+export default class Home extends Component {
+    render() {
+        return <p>Hello React</p>;
+    }
+}
